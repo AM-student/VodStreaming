8ecb1fac874e11e882114103bea3e6c437a0d9dd
AM-student <93679555+AM-student@users.noreply.github.com>
2022-12-25 16:27:57 +0100

    Add project files.

---
+++ VodStreaming/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VodStreaming.Models
+{
+    public class Category
+    {
+        [Required]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ VodStreaming/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VodStreaming.Models
+{
+    public class Video
+    {
+        [Required]
+        public int VideoID { get; set; }
+        public string Name { get; set; }
+        public string ThumbnailPath { get; set; }
+        public string? FilePath { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+        public int? CategoryID { get; set; }
+        [ForeignKey("CategoryID ")]
+        public Category? Category { get; set; }
+
+        [Required(ErrorMessage ="Please Upload the required Video")]
+        [Display(Name ="VideoFile")]
+        [NotMapped]
+        public IFormFile VideoFile { get; set; }    
+    }
+}
